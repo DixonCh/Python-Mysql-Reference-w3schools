197192a8865f026ce17722cd58de09b8514ed3ea
DixonCh <dixonchaudhary7@gmail.com>
2020-05-09 17:20:06 +0545

    Add files via upload

---
+++ 07 Python MySQL Order By.py
@@ -0,0 +1,53 @@
+Python MySQL Order By
+Sort the Result
+Use the ORDER BY statement to sort the result in ascending or descending order.
+
+The ORDER BY keyword sorts the result ascending by default. To sort the result in descending order, use the DESC keyword.
+
+Example
+Sort the result alphabetically by name: result:
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  passwd="yourpassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+sql = "SELECT * FROM customers ORDER BY name"
+
+mycursor.execute(sql)
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
+ORDER BY DESC
+Use the DESC keyword to sort the result in a descending order.
+
+Example
+Sort the result reverse alphabetically by name:
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  passwd="yourpassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+sql = "SELECT * FROM customers ORDER BY name DESC"
+
+mycursor.execute(sql)
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
