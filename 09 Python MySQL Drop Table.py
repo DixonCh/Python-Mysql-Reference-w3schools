a0eb82d6cd7e63c5586b46086bc903a89b91bd51
DixonCh <dixonchaudhary7@gmail.com>
2020-05-11 12:39:08 +0545

    Update 09 Python MySQL Drop Table.py

--- 09 Python MySQL Drop Table.py
+++ 09 Python MySQL Drop Table.py
@@ -1,10 +1,11 @@
+'''
 Python MySQL Drop Table
 Delete a Table
 You can delete an existing table by using the "DROP TABLE" statement:
 
 Example
 Delete the table "customers":
-
+'''
 import mysql.connector
 
 mydb = mysql.connector.connect(
@@ -19,12 +20,13 @@ mycursor = mydb.cursor()
 sql = "DROP TABLE customers"
 
 mycursor.execute(sql)
+'''
 Drop Only if Exist
 If the the table you want to delete is already deleted, or for any other reason does not exist, you can use the IF EXISTS keyword to avoid getting an error.
 
 Example
 Delete the table "customers" if it exists:
-
+'''
 import mysql.connector
 
 mydb = mysql.connector.connect(
