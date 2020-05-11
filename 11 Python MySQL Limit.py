7053203b0631da4969c376ec1eda7c899d21abda
DixonCh <dixonchaudhary7@gmail.com>
2020-05-11 12:44:05 +0545

    Update 11 Python MySQL Limit.py

--- 11 Python MySQL Limit.py
+++ 11 Python MySQL Limit.py
@@ -1,10 +1,11 @@
+'''
 Python MySQL Limit
 Limit the Result
 You can limit the number of records returned from the query, by using the "LIMIT" statement:
 
 Example
 Select the 5 first records in the "customers" table:
-
+'''
 import mysql.connector
 
 mydb = mysql.connector.connect(
@@ -22,12 +23,14 @@ myresult = mycursor.fetchall()
 
 for x in myresult:
   print(x)
+ 
+'''
 Start From Another Position
 If you want to return five records, starting from the third record, you can use the "OFFSET" keyword:
 
 Example
 Start from position 3, and return 5 records:
-
+'''
 import mysql.connector
 
 mydb = mysql.connector.connect(
