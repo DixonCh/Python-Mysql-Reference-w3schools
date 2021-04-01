06e5522686a9cdfeaafa42e48b05fe6d3e9610bf
Dixon Chaudhary <dixonchaudhary7@gmail.com>
2021-04-01 13:30:00 +0545

    Update 09 Python MySQL Drop Table.py

--- 09 Python MySQL Drop Table.py
+++ 09 Python MySQL Drop Table.py
@@ -26,6 +26,7 @@ sql = "DROP TABLE customers"
 
 mycursor.execute(sql)
 '''
+
 Drop Only if Exist:
 
 If the the table you want to delete is already deleted, or for any other reason does not exist, you can use the IF EXISTS keyword to avoid getting an error.
